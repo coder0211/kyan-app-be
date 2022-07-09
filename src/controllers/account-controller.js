7027f33000cd63d1214525842dd40e00e989c7dc
Nguyễn Đức Hòa <nguyenduchoa.data@gmail.com>
2022-07-09 09:10:58 +0700

    update routes handle account

--- src/controllers/account-controller.js
+++ src/controllers/account-controller.js
@@ -1,9 +1,31 @@
 module.exports = function (con) {
+  this.createOrUpdateAccount = function (req, res) {
+    var accountMail = req.body.accountMail;
+    var accountDisplayName = req.body.accountDisplayName;
+    var accountUrlPhoto = req.body.accountUrlPhoto;
+    var sql =
+      "INSERT INTO Account (accountMail, accountDisplayName, accountUrlPhoto) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE accountDisplayName = ?, accountUrlPhoto = ?";
+    con.query(
+      sql,
+      [
+        accountMail,
+        accountDisplayName,
+        accountUrlPhoto,
+        accountDisplayName,
+        accountUrlPhoto,
+      ],
+      function (err, result) {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  };
+
   this.getAccounts = function (req, res) {
-    var mail = req.query.mail;
-    if (mail != null) {
-      var sql = "SELECT * FROM Account WHERE mail = ?";
-      con.query(sql, [mail], function (err, result) {
+    var accountMail = req.query.accountMail;
+    if (accountMail != null) {
+      var sql = "SELECT * FROM Account WHERE accountMail = ?";
+      con.query(sql, [accountMail], function (err, result) {
         if (err) throw err;
         res.send(result[0]);
       });
@@ -12,6 +34,19 @@ module.exports = function (con) {
         if (err) throw err;
         res.send(result);
       });
+    }
+  };
+
+  this.deleteAccount = function (req, res) {
+    var accountMail = req.body.accountMail;
+    if (accountMail != null) {
+      sql = "DELETE FROM Account WHERE accountMail = ?";
+      con.query(sql, [accountMail], function (err, result) {
+        if (err) throw err;
+        res.send(result);
+      });
+    } else {
+      if (err) throw "Account mail can not null";
     }
   };
 };
--- src/routes/account-route.js
+++ src/routes/account-route.js
@@ -1,10 +1,28 @@
 accountRoutes = function (app, con) {
   //
+  // DECLARE ACCOUNT CONTROLLER
+  //
   var _ac = require("../controllers/account-controller");
   var accountController = new _ac(con);
+  //
+  // end declare account controller
 
-  app.get("/accounts/:mail?", function (req, res) {
+  //
+  //---     ROUTES      ---
+  //
+  app.post("/account", function (req, res) {
+    console.log("\x1b[36m", "POST:/account", "\x1b[0m");
+    accountController.createOrUpdateAccount(req, res);
+  });
+
+  app.get("/accounts/:accountMail?", function (req, res) {
+    console.log("\x1b[36m", "GET:/accounts/:accountMail?", "\x1b[0m");
     accountController.getAccounts(req, res);
+  });
+
+  app.put("/account/:accountMail?", function (req, res) {
+    console.log("\x1b[36m", "PUT:/account", "\x1b[0m");
+    accountController.deleteAccount(req, res);
   });
 };
 
