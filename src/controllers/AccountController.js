ecc76ff1447b6bd5983555ad561c011ddbeb0f15
nvduy0511 <61537028+nvduy0511@users.noreply.github.com>
2022-09-07 15:55:04 +0700

    Feature/save file to server (#4)
    * save one file
    
    * done upload file to server

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,10 +1,11 @@
 const db = require('../configs/config-mysql');
+const asyncHandler = require('express-async-handler');
 const asyncQuery = require('../helpers/async-mysql');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const Account = require('../models/account');
 class AccountController {
-    async createOrUpdate(req, res) {
+    createOrUpdate = asyncHandler(async (req, res) => {
         console.log(req.body);
         var accountMail = req.body.accountMail;
         var accountDisplayName = req.body.accountDisplayName;
@@ -12,32 +13,29 @@ class AccountController {
         var accountToken = req.body.accountToken;
         var accountId = req.body.accountId;
         var sql = `INSERT INTO Account (accountId, accountMail, accountDisplayName, accountUrlPhoto, accountToken) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE accountDisplayName = ?, accountUrlPhoto = ?`;
-        try {
-            const result = await asyncQuery(db, sql, [
+
+        const result = await asyncQuery(db, sql, [
+            accountId,
+            accountMail,
+            accountDisplayName,
+            accountUrlPhoto,
+            accountToken,
+            accountDisplayName,
+            accountUrlPhoto,
+        ]);
+        if (result) {
+            let myAccount = new Account(
                 accountId,
                 accountMail,
                 accountDisplayName,
                 accountUrlPhoto,
                 accountToken,
-                accountDisplayName,
-                accountUrlPhoto,
-            ]);
-            if (result) {
-                let myAccount = new Account(
-                    accountId,
-                    accountMail,
-                    accountDisplayName,
-                    accountUrlPhoto,
-                    accountToken,
-                );
-                res.send(myAccount);
-            }
-        } catch (error) {
-            res.json(error);
+            );
+            res.send(myAccount);
         }
-    }
+    });
 
-    login(req, res) {
+    login = asyncHandler(async (req, res) => {
         const { mail } = req.body;
         console.log(mail);
         const sql = `SELECT * FROM Account WHERE accountMail = ?`;
@@ -53,9 +51,9 @@ class AccountController {
                 },
             );
         });
-    }
+    });
 
-    getOne(req, res) {
+    getOne = asyncHandler(async (req, res) => {
         var accountMail = req.params.accountMail;
         if (accountMail != null) {
             var sql = 'SELECT * FROM Account WHERE accountMail = ?';
@@ -64,27 +62,23 @@ class AccountController {
                 res.send(result[0]);
             });
         }
-    }
+    });
 
-    getAll(req, res) {
+    getAll = asyncHandler(async (req, res) => {
         db.query('SELECT * FROM Account', function (err, result) {
             if (err) throw err;
             res.send(result);
         });
-    }
+    });
 
-    async delete(req, res) {
+    delete = asyncHandler(async (req, res) => {
         var accountMail = req.body.accountMail;
         const sql = 'DELETE FROM Account WHERE accountMail = ?';
 
-        try {
-            if (!accountMail) throw 'Account mail can not null';
-            const result = await asyncQuery(db, sql, [accountMail]);
-            res.json(result);
-        } catch (error) {
-            res.json(error);
-        }
-    }
+        if (!accountMail) throw 'Account mail can not null';
+        const result = await asyncQuery(db, sql, [accountMail]);
+        res.json(result);
+    });
 }
 
 module.exports = new AccountController();
