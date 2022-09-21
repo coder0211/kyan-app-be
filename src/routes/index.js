d0436e322792311b549e03a500cb20b3de07f9c1
nvduy0511 <61537028+nvduy0511@users.noreply.github.com>
2022-09-21 16:44:50 +0700

    hotfix auth in upload-file.route (#5)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const accountRoute = require('./account.route');
 const taskRoute = require('./task.route');
 const workSpaceRoute = require('./work-space.route');
+const uploadFile = require('./upload-file.route');
 
 router.get('/', (req, res) => {
     res.send('Welcome to kyan app!');
@@ -10,5 +11,5 @@ router.get('/', (req, res) => {
 router.use('/account', accountRoute);
 router.use('/task', taskRoute);
 router.use('/work-space', workSpaceRoute);
-
+router.use('/file', uploadFile);
 module.exports = router;
