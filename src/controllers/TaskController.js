cff3b7eb8a7f3cebbeda1d9857fbd170eec34fc4
Nguyễn Đức Hòa <nguyenduchoa.data@gmail.com>
2022-10-27 15:41:38 +0700

    [UPDATE] update get tasks with id and date

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -73,7 +73,7 @@ class TaskController {
     getAll = asyncHandler(async (req, res) => {
         const { taskAssignTo, taskDueTimeGTE } = req.query;
         const sql =
-            'SELECT * FROM Task AS T WHERE T.taskAssignTo = ? AND (DATEDIFF(DATE(T.taskDueTimeGTE), DATE(?)) >= 0)';
+            'SELECT * FROM Task AS T WHERE T.taskAssignTo = ? AND (DATEDIFF(DATE(T.taskDueTimeLTE), DATE(?)) >= 0)';
         db.query(sql, [taskAssignTo, taskDueTimeGTE], (err, result) => {
             if (err) throw err;
             res.send(result);
