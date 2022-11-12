58662f524ea66f6c4e350a48a1ef5a45c5ae8a4d
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-11-12 15:00:35 +0700

    done clone function base on old version

--- src/controllers/ConversationController.js
+++ src/controllers/ConversationController.js
@@ -26,11 +26,48 @@ class ConversationController {
         res.send(result);
     });
 
-    getAll = asyncHandler(async (req, res) => {
-        db.query('SELECT * FROM Conversation', function (err, result) {
-            if (err) throw err;
-            res.send(result);
+    getMemberConversationByIdConversation = function (conversationId, accountId) {
+        return new Promise((resolve) => {
+            db.query(
+                'SELECT A.* FROM ConversationMember M LEFT JOIN Account A ON M.accountId = A.accountId WHERE M.conversationId = ? AND EXISTS (SELECT * FROM ConversationMember MC WHERE MC.conversationId = ? AND MC.accountId = ?)',
+                [conversationId, conversationId, accountId],
+                function (err, result) {
+                    if (err) throw err;
+                    resolve(result);
+                },
+            );
         });
+    };
+
+    getAll = asyncHandler(async (req, res) => {
+        var conversationWorkspaceId = req.query.conversationWorkspaceId;
+        var accountId = req.query.accountId;
+        if (conversationWorkspaceId != null) {
+            var getMemberConversationByIdConversation = this.getMemberConversationByIdConversation;
+            db.query(
+                'SELECT * FROM Conversation WHERE conversationWorkspaceId = ?',
+                [conversationWorkspaceId],
+                async function (err, result) {
+                    if (err) throw err;
+                    if (result.length > 0) {
+                        var list = [],
+                            index = 0;
+                        for (let i = 0; i < result.length; i++) {
+                            var element = result[i];
+                            var listMember = await getMemberConversationByIdConversation(
+                                element.conversationId,
+                                accountId,
+                            );
+                            if (listMember.length > 0) {
+                                list.push(element);
+                                list[index++]['listMember'] = listMember;
+                            }
+                        }
+                        res.send(list);
+                    } else res.send([]);
+                },
+            );
+        } else res.send([]);
     });
 
     delete = asyncHandler(async (req, res) => {
